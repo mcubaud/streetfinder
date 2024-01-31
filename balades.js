0f6a4bf8507bdc429d0e50b44f4b711cae99ce1e
mcubaud <77577491+mcubaud@users.noreply.github.com>
2024-01-31 15:27:44 +0000

    ajout de la sauvegarde

--- balades.js
+++ balades.js
@@ -232,3 +232,17 @@ function onLocationError(e) {
 }
 
 mymap.on('locationerror', onLocationError);
+
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+document.getElementById("save").onclick = function(){
+    downloadObjectAsJson(trouves, "streetfinder_save_"+lieu);
+}
