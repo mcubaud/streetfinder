3ea96d56cf62954ee2c8d44cb5b3b327e7825100
mcubaud <77577491+mcubaud@users.noreply.github.com>
2024-02-06 13:14:36 +0000

    test

--- balades.js
+++ balades.js
@@ -153,11 +153,11 @@ function chercher(){
                     find_length += polylines[i].length;
                     trouves[i] = true;
                     if (!trouves_uniques.includes(name2)){
-                        var name_std = standardize(name2).replaceAll("' ","_").replaceAll(" ","_")
+                        let name_std = standardize(name2).replaceAll("' ","_").replaceAll(" ","_")
                         document.getElementById("list_found").innerHTML+="<li id='list_found_"+name_std+"'>"+name2+"</li>";
                         let thispolyline = polylines[i]
                         document.getElementById("list_found_"+name_std).onclick=function(){
-                            console.log(name2);
+                            console.log(name_std);
                             thispolyline.openTooltip();
                         }
                         trouves_uniques.push(name2);
@@ -302,11 +302,11 @@ function upload_save(saved_trouvees){
                 trouves[i] = true;
             }
             if (!trouves_uniques.includes(name2)){
-                var name_std = standardize(name2).replaceAll("' ","_").replaceAll(" ","_")
+                let name_std = standardize(name2).replaceAll("' ","_").replaceAll(" ","_")
                 document.getElementById("list_found").innerHTML+="<li id='list_found_"+name_std+"'>"+name2+"</li>";
                 let thispolyline = polylines[i]
                 document.getElementById("list_found_"+name_std).onclick=function(){
-                    console.log(name2);
+                    console.log(name_std);
                     thispolyline.openTooltip();
                 }
                 trouves_uniques.push(name2);
